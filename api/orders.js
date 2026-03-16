3d70b236efe5f0e95e62609864395144429e8fc6
dimadeals <dimadeals0@gmail.com>
2026-03-16 13:55:13 +0100

    Add files via upload

--- api/orders.js
+++ api/orders.js
@@ -2,6 +2,12 @@ import redis from "./redis.js";
 
 export default async function handler(req, res) {
 
+  const adminKey = req.headers.authorization;
+
+  if (adminKey !== process.env.ADMIN_KEY) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
   try {
 
     const keys = await redis.keys("order:*");
